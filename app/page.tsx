2b71f969ca3bbcc194ca0c284d0a1d1b21e2cdcd
Nabil <nabil_ee@hotmail.com>
2024-10-22 11:00:49 +0200

    add projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Header from "@/components/sections/header";
-import HeroSection from "@/components/sections/hero";
+import Header from "@/components/sections/header"
+import HeroSection from "@/components/sections/hero"
+import ProjectsSection from "@/components/sections/projects"
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <ProjectsSection />
     </div>
   )
 }
---
+++ constants/index.ts
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        company: "CEMI",
+        year: "2023",
+        title: "Enrollment App",
+        results: [
+            { title: "" },
+            { title: "" },
+            { title: "" }
+        ],
+        link: "",
+        image: ""
+    },
+    {
+        company: "Himmah",
+        year: "2024",
+        title: "Focus App",
+        results: [
+            { title: "" },
+            { title: "" },
+            { title: "" }
+        ],
+        link: "",
+        image: ""
+    },
+    {
+        company: "CEMI",
+        year: "2023",
+        title: "Enrollment App",
+        results: [
+            { title: "" },
+            { title: "" },
+            { title: "" }
+        ],
+        link: "",
+        image: ""
+    }
+]
