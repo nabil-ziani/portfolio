9af7d70708664554d43009a4ef98f0cb3ccd14c1
Nabil <nabil_ee@hotmail.com>
2024-10-28 11:31:28 +0100

    animate about section

--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Card from "@/components/card"
 import SectionHeader from "@/components/section-header"
 import bookImage from "@/assets/images/book-cover.png"
@@ -7,8 +9,13 @@ import mapImage from "@/assets/images/map.png"
 import smileMemoji from "@/assets/images/memoji-smile.png"
 import CardHeader from "@/components/card-header"
 import ToolboxItems from "@/components/toolbox-items"
+import { motion } from "framer-motion"
+import { useRef } from "react"
 
 const AboutSection = () => {
+
+    const constraintRef = useRef(null)
+
     return (
         <section className="py-20 lg:py-28">
             <div className="container">
@@ -25,20 +32,26 @@ const AboutSection = () => {
 
                         <Card className="h-[320px] md:col-span-3 lg:col-span-2">
                             <CardHeader title="My Toolbox" description="Explore the technologies and tools I use to craft exceptional digital experiences." />
-                            <ToolboxItems items={toolboxItems} />
-                            <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2" />
+                            <ToolboxItems items={toolboxItems} itemsWrapperClassName="animate-move-left [animation-duration:30s]" />
+                            <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="animate-move-right [animation-duration:15s]" />
                         </Card>
                     </div>
 
                     <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
                         <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
                             <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." className="px-6 py-6" />
-                            <div className="relative flex-1">
+                            <div className="relative flex-1" ref={constraintRef}>
                                 {hobbies.map((hobby, index) => (
-                                    <div key={index} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-indigo-400 to-sky-300 rounded-lg py-1.5 absolute" style={{ left: hobby.left, top: hobby.top }}>
+                                    <motion.div
+                                        key={index}
+                                        className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-indigo-400 to-sky-300 rounded-lg py-1.5 absolute"
+                                        style={{ left: hobby.left, top: hobby.top }}
+                                        drag
+                                        dragConstraints={constraintRef}
+                                    >
                                         <span className="font-medium text-gray-950">{hobby.title}</span>
                                         <span>{hobby.emoji}</span>
-                                    </div>
+                                    </motion.div>
                                 ))}
                             </div>
                         </Card>
@@ -48,6 +61,8 @@ const AboutSection = () => {
                             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full 
                         bg-gradient-to-r from-indigo-400 to-sky-300 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2
                         after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-indigo-400 to-sky-300 -z-20 animate-ping [animation-duration:2s]" />
+                                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-indigo-400 to-sky-300 -z-10" />
                                 <Image src={smileMemoji} alt="Smiling emoji" className="size-20" />
                             </div>
                         </Card>
--- components/toolbox-items.tsx
+++ components/toolbox-items.tsx
@@ -1,4 +1,5 @@
 import TechIcon from "@/components/tech-icon"
+import { Fragment } from "react"
 import { twMerge } from "tailwind-merge"
 
 type ToolboxItem = {
@@ -16,11 +17,15 @@ const ToolboxItems = ({ items, className, itemsWrapperClassName }: ToolboxItemsP
     return (
         <div className={twMerge("flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]", className)}>
             <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWrapperClassName)}>
-                {items.map((item, index) => (
-                    <div key={index} className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
-                        <TechIcon component={item.iconType} />
-                        <span className="font-semibold">{item.title}</span>
-                    </div>
+                {[...new Array(2)].fill(0).map((_, index) => (
+                    <Fragment key={index}>
+                        {items.map((item, index) => (
+                            <div key={index} className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg">
+                                <TechIcon component={item.iconType} />
+                                <span className="font-semibold">{item.title}</span>
+                            </div>
+                        ))}
+                    </Fragment>
                 ))}
             </div>
         </div>
