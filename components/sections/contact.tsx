58ab2e60b1fa078fd7731ae57ad6e5d5f20d9356
Nabil <nabil_ee@hotmail.com>
2024-10-28 11:40:13 +0100

    fix build

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -9,9 +9,9 @@ const ContactSection = () => {
                     <div className="absolute inset-0 -z-10 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }} />
                     <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
                         <div>
-                            <h2 className="font-serif text-2xl md:text-3xl">Let's create something amazing together</h2>
+                            <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s create something amazing together</h2>
                             <p className="text-sm md:text-base mt-2">
-                                Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
+                                Ready to bring your next project to life? Let&apos;s connect and discuss how I can help you achieve your goals.
                             </p>
                         </div>
                         <div>
